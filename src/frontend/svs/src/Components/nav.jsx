40afcee4b7185600cc1292cf94be65edee9f57d7
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 01:16:42 -0400

    created navigation frontend component

---
+++ src/frontend/svs/src/Components/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default class Nav extends React.Component{
+
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
