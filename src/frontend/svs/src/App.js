2f8d99b16761ee3a659e3c878cffe344ca880d50
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 11:40:50 -0400

    updated app.js for navbar

--- src/frontend/svs/src/App.js
+++ src/frontend/svs/src/App.js
@@ -1,26 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import './App.css'
-import Home from './Components/home'
-import Api from './Components/api'
-import Upload from './Components/upload'
-import Nav from './Components/nav'
-import Footer from './Components/footer'
+import React from "react";
+import "./App.css";
+import Navigation from "./Components/nav";
 
-export default class App extends React.Component{
-
-  render(){
+export default class App extends React.Component {
+  render() {
     return (
       <div className="App">
-        <Router>
-          <Nav/>
-          <div className="content">
-            <Route path="/" component={Home}/>
-            <Route path="/apiPage" component={Api}/>
-            <Route path="/upload" component={Upload}/>
-          </div>
-          <Footer/>
-        </Router>
+        <Navigation />
       </div>
     );
   }
