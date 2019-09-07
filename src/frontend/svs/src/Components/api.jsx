31f87939b7f2d68d94414e8a33d97de602a5e1c6
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 16:34:44 -0400

    updated api page

--- src/frontend/svs/src/Components/api.jsx
+++ src/frontend/svs/src/Components/api.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import css from './nav.css';
+// import './nav.css';
 
 export default class Api extends React.Component{
 
   render(){
+    console.log("API Page loaded")
     return (
-      <div className={css.nav}>
+      <div className="container">
         <div className="title">
           How to use SVS
         </div>
-        <div className="body">
-          
+        <div className="contentText">
+          In order to use our api you'll have to convert your image to <code>base64</code> and call <code>svisions.tech/api</code>
         </div>
       </div>
     );
