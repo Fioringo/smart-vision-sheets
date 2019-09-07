5b5bd37e205162b667cee33f8fd00e458016a366
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 16:34:35 -0400

    updated about

--- src/frontend/svs/src/Components/about.jsx
+++ src/frontend/svs/src/Components/about.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './about.css';
+import React from "react";
+import "./about.css";
 
-export default class About extends React.Component{
-
-  render(){
+export default class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
     return (
-      <div className="footer">
-        <div className="about">
-          This project was created to erase the pain of entering values manually into a google spreadsheet.
+      <div className="about">
+        <div className="title">
+          <div className="yellow">A</div>bout
+        </div>
+        <div className="aboutText">
+          This project was created to erase the pain of entering values manually
+          into a google spreadsheet.
         </div>
       </div>
     );
