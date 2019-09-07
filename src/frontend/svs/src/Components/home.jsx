537bd11f730a32e5a888f71523a3431082afa598
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 17:28:17 -0400

    added more info about SVS

--- src/frontend/svs/src/Components/home.jsx
+++ src/frontend/svs/src/Components/home.jsx
@@ -20,6 +20,28 @@ export default class Home extends React.Component {
         <div className="description">
           Convert your sheets into Google Sheets
         </div>
+        <br/>
+        <div className="smallText">
+          Converting your hand written sheets is easy with 4 simple steps:
+        </div>
+        <div className="homeText">
+          <div className="step">
+            <div className="stepTitle">{"Step" } <div className="yellow">1</div>:</div>
+            <div className="stepText">Login with your Google Account</div>
+          </div>
+          <div className="step">
+            <div className="stepTitle">{"Step" } <div className="blue">2</div>:</div>
+            <div className="stepText">Take a picture of the desired sheet to become a google sheet!</div>
+          </div>
+          <div className="step">
+            <div className="stepTitle">{"Step"}<div className="red">{"3"}</div>:</div>
+            <div className="stepText">Upload the image here!</div>
+          </div>
+          <div className="step">
+            <div className="stepTitle">{"Step" } <div className="green">4</div>:</div>
+            <div className="stepText">Enjoy your Google Sheet!</div>
+          </div>
+        </div>
       </div>
     );
   }
