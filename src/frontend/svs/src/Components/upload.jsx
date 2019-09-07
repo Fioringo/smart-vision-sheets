680db40fe918bc6a5560ced72a558ab741518446
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 01:16:53 -0400

    created upload frontend component

---
+++ src/frontend/svs/src/Components/upload.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import css from './App.css';
+
+export default class Upload extends React.Component{
+
+  render(){
+    return (
+      <div className={css.upload}>
+        
+      </div>
+    );
+  }
+}
