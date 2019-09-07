42ec3aa15bc1da7ce54565cb647053bc072211a9
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 17:09:57 -0400

    added more info on api

--- src/frontend/svs/src/Components/api.jsx
+++ src/frontend/svs/src/Components/api.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 // import './nav.css';
 
-export default class Api extends React.Component{
-
-  render(){
-    console.log("API Page loaded")
+export default class Api extends React.Component {
+  render() {
+    console.log("API Page loaded");
     return (
       <div className="container">
-        <div className="title">
-          How to use SVS
-        </div>
+        <div className="title">How to use SVS</div>
         <div className="contentText">
-          In order to use our api you'll have to convert your image to <code>base64</code> and call <code>svisions.tech/api</code>
+          <div className="block">
+            <div className="apiText">
+              In order to use our api you'll have to convert your image to{" "}
+              <code>base64</code> and call{" "}
+              <code>https://svisions.tech/api/image</code>
+            </div>
+            <div className="apiExample">
+              <div className="language">
+                JavaScript
+              </div>
+              <code>
+                ...<br/>
+                axios.post('https://svisions.tech/api/image',image)<br/>
+                ...<br/>
+              </code>
+            </div>
+          </div>
+          <br />
         </div>
       </div>
     );
