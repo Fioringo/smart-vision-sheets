2b2d7303f56e9432e999cc460903d0fbbd57f9cc
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 16:35:41 -0400

    added more components to nav

--- src/frontend/svs/src/Components/nav.jsx
+++ src/frontend/svs/src/Components/nav.jsx
@@ -3,18 +3,38 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./nav.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Home from './home'
-import Api from './api'
-import Footer from './footer'
+import Home from "./home";
+import Api from "./api";
+import Footer from "./footer";
 import Upload from "./upload";
+import About from "./about";
+import axios from "axios";
+import Privacy from "./privacy";
+import SheetResult from "./result";
 
 export default class Navigation extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      activeKey: "Home"
+      activeKey: "Home",
+      auth: null
     };
+  }
+
+  componentDidMount() {
+    // console.log("header mounted");
+    axios
+      .get("/auth/current_user")
+      .then(response => {
+        console.log("response from auth/google", response);
+        this.setState({ auth: response.data.userData });
+        console.log("auth: ", this.state.auth);
+        console.log(typeof this.state.auth);
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 
   handleSelect = page => this.setState({ activeKey: page });
@@ -38,6 +58,7 @@ export default class Navigation extends React.Component {
             <Nav className="mr-auto">
               <Nav.Link
                 className="item"
+                eventKey="API"
                 onSelect={this.handleSelect}
                 as={Link}
                 to="/api"
@@ -46,21 +67,33 @@ export default class Navigation extends React.Component {
               </Nav.Link>
               <Nav.Link
                 className="item"
+                eventKey="Upload"
                 onSelect={this.handleSelect}
                 as={Link}
                 to="/upload"
               >
                 Upload
               </Nav.Link>
+              <Nav.Link
+                className="item"
+                eventKey="Upload"
+                onSelect={this.handleSelect}
+                href={this.state.auth ? "/auth/logout" : "/auth/google"}
+              >
+                {this.state.auth ? "Logout" : "Google Login"}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
         <div className="content">
-            <Route path="/" component={Home}/>
-            <Route path="/apiPage" component={Api}/>
-            <Route path="/upload" component={Upload}/>
-          </div>
-          <Footer/>
+          <Route path="/" exact component={Home} />
+          <Route path="/api" component={Api} />
+          <Route path="/upload" component={Upload} />
+          <Route path="/about" component={About} />
+          <Route path="/privacy" component={Privacy} />
+          <Route path="/results" component={SheetResult} />
+        </div>
+        <Footer />
       </Router>
     );
   }
