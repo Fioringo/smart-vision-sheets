2b4ccd03a412794866aa01c9b5ab077445932e36
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 11:41:01 -0400

    updated navbar to navigate all site

--- src/frontend/svs/src/Components/nav.jsx
+++ src/frontend/svs/src/Components/nav.jsx
@@ -1,16 +1,67 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
-import './nav.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./nav.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Home from './home'
+import Api from './api'
+import Footer from './footer'
+import Upload from "./upload";
 
-export default class Nav extends React.Component{
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
 
-  render(){
+    this.state = {
+      activeKey: "Home"
+    };
+  }
+
+  handleSelect = page => this.setState({ activeKey: page });
+
+  render() {
     return (
-      <div className="nav">
-        <Link to="/"><img className="logo" src="../svslogo.svg" alt="logo"></img></Link>
-        <Link to="/Api" className="item">Api</Link>
-        <Link to="/Upload" className="item">Upload</Link>
-      </div>
+      <Router>
+        <Navbar
+          bg="dark"
+          variant="dark"
+          expand="lg"
+          fixed="top"
+          collapseOnSelect
+          className="nav-bar"
+        >
+          <Navbar.Brand as={Link} to="/">
+            <div className="logoImage" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link
+                className="item"
+                onSelect={this.handleSelect}
+                as={Link}
+                to="/api"
+              >
+                API
+              </Nav.Link>
+              <Nav.Link
+                className="item"
+                onSelect={this.handleSelect}
+                as={Link}
+                to="/upload"
+              >
+                Upload
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="content">
+            <Route path="/" component={Home}/>
+            <Route path="/apiPage" component={Api}/>
+            <Route path="/upload" component={Upload}/>
+          </div>
+          <Footer/>
+      </Router>
     );
   }
 }
