05960d3be371674551746c999df754b4769193d7
Fioringo <shtuk.andrey@gmail.com>
2019-09-07 13:04:57 -0400

    trying to make fetch work

--- src/frontend/svs/src/Components/upload.jsx
+++ src/frontend/svs/src/Components/upload.jsx
@@ -1,53 +1,74 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 export default class Upload extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        selectedFile: null
-      }
-   
+    this.state = {
+      selectedFile: null,
+      loaded: 0,
+      loading: false,
+      responseSuccess: "",
+      response: {},
+    };
   }
 
-  onChangeHandler=event=>{
+  onChangeHandler = event => {
     this.setState({
       selectedFile: event.target.files[0],
-      loaded: 0,
+      loaded: 0
+    });
+  };
+
+  onClickHandler = async () => {
+    let imageBinary
+    const data = new FormData()
+    data.append('file', this.state.selectedFile, this.state.selectedFile.name)
+    imageBinary = await fetch(this.state.selectedFile)
+    // imageBinary = new Buffer.from(imageBinary, 'binary').toString('base64')
+    console.log(this.state.selectedFile)
+    console.log(imageBinary)
+    // console.log(imageBinary)
+    return 0
+    axios.post('/process_image',{image: imageBinary})
+    .then((response) => {
+      this.setState({
+        loading: false,
+        responseSuccess: response.data.success,
+        response: response.data
+      })
     })
-  }
 
-  onClickHandler = () => {
-    const data = new FormData() 
-    data.append('file', this.state.selectedFile)
-    axios.post("http://localhost:8000/upload", data, { // receive two parameter endpoint url ,form data 
-      })
-      .then(res => { // then print response status
-        console.log(res.statusText)
-      })
-}
-
+    // axios.post("http://localhost:8000/upload", data, { // receive two parameter endpoint url ,form data
+    //   })
+    //   .then(res => { // then print response status
+    //     console.log(res.statusText)
+    // })
+  };
 
   render() {
     return (
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
             <form method="post" action="#" id="#">
-              <div class="form-group files">
+              <div className="form-group files color">
                 <label>Upload Your File </label>
-                <input type="file" class="form-control" multiple=""/>
+                <input
+                  type="file"
+                  className="form-control"
+                  multiple
+                  onChange={this.onChangeHandler}
+                />
               </div>
             </form>
-          </div>
-            <div class="col-md-6">
-              <form method="post" action="#" id="#">
-                <div class="form-group files color">
-                  <label>Upload Your File </label>
-                  <input type="file" class="form-control" multiple onChange={this.onChangeHandler}/>
-                </div>
-              </form>
-              <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
+            <button
+              type="button"
+              className="btn btn-success btn-block"
+              onClick={this.onClickHandler}
+            >
+              Upload
+            </button>
           </div>
         </div>
       </div>
