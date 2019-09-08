d8de56ae1c1b01ff9bdda8cb788a66320168cf4d
Fioringo <shtuk.andrey@gmail.com>
2019-09-08 01:19:29 -0400

    added csv downloader

--- src/frontend/svs/src/Components/result.jsx
+++ src/frontend/svs/src/Components/result.jsx
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import CSVLogo from "../images/csv-file-format-extension.svg";
 import GoogleSheets from "../images/Google sheets.svg";
 import Axios from "axios";
+import CsvDownloader from "react-csv-downloader";
 
 export default class SheetResult extends React.Component {
   constructor(props) {
@@ -16,13 +17,9 @@ export default class SheetResult extends React.Component {
     this.props.startAgain();
   };
 
-  downloadCSV = () => {
-    
-  }
-
   createSheet = () => {
-    Axios.post('',this.props.data)
-  }
+    Axios.post("", this.props.data);
+  };
 
   render() {
     return (
@@ -34,23 +31,36 @@ export default class SheetResult extends React.Component {
           Here are the results! How would you like to save them?
         </div>
         <div className="options">
-          <Card bg="info" style={{ width: "12rem" , margin: "0.4em", padding: "0.3em"}} className="text-center">
+          <Card
+            bg="info"
+            style={{ width: "12rem", margin: "0.4em", padding: "0.3em" }}
+            className="text-center"
+          >
             <Card.Img variant="top" src={CSVLogo} />
             <Card.Body>
               <Card.Title>CSV File</Card.Title>
-              <Button variant="warning">Download</Button>
+              <CsvDownloader>
+                <Button variant="warning">Download</Button>
+              </CsvDownloader>
             </Card.Body>
           </Card>
-          <Card bg="primary" style={{ width: "12rem" , margin: "0.4em", padding: "0.2em"}} className="text-center">
+          <Card
+            bg="primary"
+            style={{ width: "12rem", margin: "0.4em", padding: "0.2em" }}
+            className="text-center"
+          >
             <Card.Img variant="top" src={GoogleSheets} />
             <Card.Body>
               <Card.Title>Google Sheets</Card.Title>
-              <Button variant="warning" onClick={this.createSheet}>Add to Sheets</Button>
-              
+              <Button variant="warning" onClick={this.createSheet}>
+                Add to Sheets
+              </Button>
             </Card.Body>
           </Card>
         </div>
-        <Button onClick={this.startAgain} variant="danger"><span className="glyphicon glyphicon-repeat"></span>Start Over</Button>
+        <Button onClick={this.startAgain} variant="danger">
+          <span className="glyphicon glyphicon-repeat"></span>Start Over
+        </Button>
       </div>
     );
   }
