7876358dc3b23c812fafd1c5bd5b1980be071b50
Andrey Shtukenberg <38798708+Fioringo@users.noreply.github.com>
2019-09-08 06:57:56 -0400

    Frontend merger (#5)
    * did create react app
    
    * updated app js to be better
    
    * created api frontend component
    
    * created home frontend component
    
    * created navigation frontend component
    
    * created upload frontend component
    
    * Added upload functionality
    
    * added font to the index
    
    * added the svs logo
    
    * updated app.css
    
    * added routers to app.js
    
    * added about.css style
    
    * added about.jsx contents
    
    * added footer css
    
    * added footer jsx
    
    * added home css
    
    * added content to home.jsx
    
    * added nav css
    
    * added nav jsx
    
    * added upload css
    
    * updated some packages
    
    * added api jsx
    
    * installed axios
    
    * moved the logo to src folder
    
    * added general text color white for ease
    
    * few eh css
    
    * moved the title to the middle
    
    * reformatted
    
    * added logo and item
    
    * moved the image
    
    * fixed items in nav
    
    * rudimentary server/backend setup
    
    * added react-bootstrap
    
    * added stylesheet to index.html
    
    * fixed content container
    
    * fixed background color
    
    * fixed navbar styling
    
    * updated app.js for navbar
    
    * updated navbar to navigate all site
    
    * Fixed footer
    
    * trying to make fetch work
    
    * added logos in png
    
    * added content centering
    
    * added drop upload
    
    * clean up
    
    * updated about
    
    * updated api page
    
    * updated github link in footer
    
    * centered home
    
    * removed unnecessary link
    
    * updated logo
    
    * added more components to nav
    
    * added privacy page
    
    * centered upload window
    
    * added result page
    
    * attempted at making upload work
    
    * added ds store ignore for .gitignore
    
    * image processing for sheets, progress on gSuiteClient
    
    * added more info on api
    
    * updated upload
    
    * Updated styling for home
    
    * added more info about SVS
    
    * added privacy css
    
    * added underline for hovering on links
    
    * modified footer to look nicer
    
    * updated privacy policy
    
    * slashes
    
    * added form data and multer
    
    * updated divs
    
    * removed warnings
    
    * added margins to colored letters
    
    * more image uploader stuff
    
    * added try method fro vision client
    
    * stuff
    
    * added append field and others
    
    * added multer and upload for multer
    
    * added loading
    
    * added feature page
    
    * fixed footer links
    
    * added loading screen
    
    * added feature page to nav
    
    * added result
    
    * added more to uploads
    
    * added google sheets and csv logos
    
    * added sheet editing functions
    
    * added loading to upload
    
    * pruned
    
    * Changed style
    
    * idk some package thing
    
    * update for correct reference
    
    * added authentication things
    
    * added logout page
    
    * removed useless things
    
    * cleaned out
    
    * installed csv downloader
    
    * added csv downloader
    
    * More style stuff
    
    * removed csv downloader due to it crashing
    
    * updated upload
    
    * moved square logo
    
    * updated index for svs
    
    * added loading animatino
    
    * added loading and button disable
    
    * Little tweaks
    
    * fixed feature page update
    
    * added request for older files
    
    * More tweaks
    
    * ew
    
    * updated dependencies
    
    * updated domain
    
    * added csv downloader
    
    * added userId magic
    
    * added more userId
    
    * added more spreadsheet creation
    
    * added methods for getting and creating new spreadsheets
    
    * added login link to instructions
    
    * added a few checks to results
    
    * updating states
    
    * updated

--- src/frontend/svs/src/Components/result.jsx
+++ src/frontend/svs/src/Components/result.jsx
@@ -5,23 +5,140 @@ import Button from "react-bootstrap/Button";
 import CSVLogo from "../images/csv-file-format-extension.svg";
 import GoogleSheets from "../images/Google sheets.svg";
 import Axios from "axios";
-import { CSVLink, CSVDownload } from "react-csv"
+import { CSVLink } from "react-csv";
+const ListGroup = require("react-bootstrap").ListGroup;
+const Alert = require("react-bootstrap").Alert;
+const Form = require("react-bootstrap").Form;
+const BASE_DOMAIN =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
 
 export default class SheetResult extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      previousProjects: [],
+      data: [],
+      fileName: "",
+      loggedIn: true, //here
+      sheetCreated: null,
+      googleName: "",
+      csvName: ""
+    };
   }
+
+  componentDidMount = () => {
+    if (localStorage.getItem("accessToken") !== null) {
+      this.setState({
+        userId: localStorage.getItem("userId"),
+        loggedIn: true
+      });
+    }
+    if (this.props.data) {
+      this.setState({
+        data: this.props.data
+      });
+    }
+    Axios.post(`${BASE_DOMAIN}/get_spreadsheets`, {
+      userId: localStorage.getItem("userId")
+    })
+      .then(res => {
+        this.setState({
+          previousProjects: res
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err,
+          previousProjects: []
+        });
+      });
+  };
 
   startAgain = () => {
     this.props.startAgain();
   };
 
   createSheet = () => {
-    Axios.post("", this.props.data);
+    let config = {
+      filename: this.state.fileName,
+      content: this.state.data,
+      userId: localStorage.getItem("userId")
+    };
+    Axios.post(`${BASE_DOMAIN}/add_spreadsheet`, this.props.data)
+      .then(res => {
+        this.setState({
+          sheetCreated: true
+        })
+      })
+      .catch(err => {
+        this.setState({
+          sheetCreated: false,
+        })
+        console.log(err)
+      });
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
   render() {
+    let previousSheets;
+    if (this.state.previousProjects !== []) {
+      var prevProjTemp = this.state.previousProjects;
+      previousSheets = prevProjTemp.map(e => {
+        return (
+          <ListGroup.Item key={e.name} action href={e.link}>
+            {e.name}
+          </ListGroup.Item>
+        );
+      });
+    }
+
+    let AddToGoogleSheet = (
+      <Card
+        bg="primary"
+        style={{ width: "12rem", margin: "0.4em", padding: "0.2em" }}
+        className="text-center"
+      >
+        <Card.Img variant="top" src={GoogleSheets} />
+        <Card.Body>
+          <Card.Title>Google Sheets</Card.Title>
+          <Form.Control
+            type="text"
+            placeholder="Sheet Name"
+            onChange={this.handleChange}
+            name="googleName"
+            value={this.state.googleName}
+            style={{ marginBottom: "0.2em" }}
+          />
+          <Button variant="warning" onClick={this.createSheet}>
+            Add to Sheets
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+
+    let GoogleSuccessAlert = (
+      <Alert variant="success">
+        <Alert.Heading
+          style={{
+            width: "12rem",
+            height: "25rem",
+            margin: "0.4em",
+            padding: "0.2em"
+          }}
+        >
+          Success!
+        </Alert.Heading>
+        <p>
+          A Google Spreadsheet has been created with the name{" "}
+          {this.state.fileName}.
+        </p>
+      </Alert>
+    );
     return (
       <div className="about">
         <div className="title">
@@ -33,34 +150,51 @@ export default class SheetResult extends React.Component {
         <div className="options">
           <Card
             bg="info"
-            style={{ width: "12rem", margin: "0.4em", padding: "0.3em" }}
+            style={{
+              width: "12rem",
+              marginRight: "0.4em",
+              padding: "0.3em",
+              margin: "auto"
+            }}
             className="text-center"
           >
-            <Card.Img variant="top" src={CSVLogo} />
+            <Card.Img
+              variant="top"
+              src={CSVLogo}
+              style={{ marginTop: "0.0em" }}
+            />
             <Card.Body>
               <Card.Title>CSV File</Card.Title>
-              {/* {this.props.data !== null ? <CSVLink data={this.props.data}>Download</CSVLink> : null} */}
-                <Button variant="warning">Download</Button>
-
+              <Form.Control
+                type="text"
+                placeholder="File Name"
+                onChange={this.handleChange}
+                name="csvName"
+                value={this.state.csvName}
+                style={{ marginBottom: "0.2em", marginTop: "0.2em" }}
+              />
+              {this.state.data !== null ? (
+                <CSVLink data={this.state.data} filename={this.state.csvName + ".csv"}>
+                  <Button variant="warning">Download CSV</Button>
+                </CSVLink>
+              ) : null}
             </Card.Body>
           </Card>
-          <Card
-            bg="primary"
-            style={{ width: "12rem", margin: "0.4em", padding: "0.2em" }}
-            className="text-center"
-          >
-            <Card.Img variant="top" src={GoogleSheets} />
-            <Card.Body>
-              <Card.Title>Google Sheets</Card.Title>
-              <Button variant="warning" onClick={this.createSheet}>
-                Add to Sheets
-              </Button>
-            </Card.Body>
-          </Card>
+          {this.state.loggedIn
+            ? this.state.sheetCreated
+              ? GoogleSuccessAlert
+              : AddToGoogleSheet
+            : null}
         </div>
         <Button onClick={this.startAgain} variant="danger">
           <span className="glyphicon glyphicon-repeat"></span>Start Over
         </Button>
+        <ListGroup defaultActiveKey="/results">{previousSheets}</ListGroup>
+        {this.state.sheetCreated === false ? (
+          <Alert variant="danger" dismissible={true} onClose={this.resetAlert}>
+            Error: Sheet wasn't created!
+          </Alert>
+        ) : null}
       </div>
     );
   }
--- src/frontend/svs/src/Components/upload.jsx
+++ src/frontend/svs/src/Components/upload.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ImageUploader from "react-images-upload";
 import "./upload.css";
-import FormData from 'form-data'
-import Loading from './loading'
+import FormData from "form-data";
+import Loading from "./loading";
+import { Alert } from "react-bootstrap";
+const BASE_DOMAIN = process.env.NODE_ENV === "production" ? "" : "http://localhost:5000"
 
 export default class Upload extends React.Component {
   constructor(props) {
@@ -13,7 +15,9 @@ export default class Upload extends React.Component {
       loading: false,
       responseSuccess: "",
       response: {},
-      hasTitle: false
+      hasTitle: false,
+      disableUpload: false,
+      uploaded: null
     };
   }
 
@@ -38,58 +42,70 @@ export default class Upload extends React.Component {
 
   onClickHandler = () => {
     this.setState({
-      loading: true,
-    })
+      disableUpload: false,
+      loading: true
+    });
     const formData = new FormData();
     console.log(this.state.selectedFile);
     if (this.state.selectedFile) {
-      formData.append('file0', this.state.selectedFile);
+      formData.append("file0", this.state.selectedFile);
     }
 
-    formData.append('hasTitle', this.state.hasTitle)
+    formData.append("hasTitle", this.state.hasTitle);
 
-    console.log(formData)
+    console.log(formData);
 
-    fetch( 'http://localhost:5000/process_image', {
-      method: 'POST',
+    fetch(`${BASE_DOMAIN}/process_image`, {
+      method: "POST",
       headers: {
-        'Accept': 'application/json',
+        Accept: "application/json"
+        // 'Content-Type': 'multipart/form-data',
       },
       body: formData
-     })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      // Perform success response.
-      this.setState({
-        loading: false
-      })
-      this.props.update(responseJson)
     })
-    .catch((error) => {
-      this.setState({
-        loading: false
+      .then(response => response.json())
+      .then(responseJson => {
+        // Perform success response.
+        this.setState({
+          disableUpload: true,
+          loading: false
+        });
+        this.props.update(responseJson);
+        console.log(responseJson);
       })
-      console.log(error)
-      console.log(["Ops, something Went Wrong."]);
-    });
+      .catch(error => {
+        this.setState({
+          disableUpload: true,
+          loading: false,
+          uploaded: false
+        });
+        console.log(error);
+        console.log(["Ops, something Went Wrong."]);
+      });
   };
 
   onDrop = e => {
-    let selectIndex
-    if(this.state.selectedFile != null){
-      selectIndex = this.state.selectedFile.length - 1
+    let selectIndex;
+    if (this.state.selectedFile != null) {
+      selectIndex = this.state.selectedFile.length - 1;
     } else {
-      selectIndex = 0
+      selectIndex = 0;
     }
     this.setState({
-      selectedFile: e[selectIndex]
+      selectedFile: e[selectIndex],
+      disableUpload: true
+    });
+  };
+
+  resetAlert = () => {
+    this.setState({
+      uploaded: null
     });
   };
 
   render() {
     return (
       <div className="container">
-      {this.state.loading ? <Loading/> : null}
         <div className="title">
           <div className="yellow">U</div>pload <div className="blue">Y</div>our{" "}
           <div className="red"> I</div>mage!
@@ -106,22 +122,49 @@ export default class Upload extends React.Component {
               {"  Has Title"}
             </div>
             <ImageUploader
+              className="grey"
               withIcon={true}
               onChange={this.onDrop}
               imgExtension={[".jpg", ".png"]}
-              accept={'accept=jpg,png'}
-              label={'Max file size: 5mb, accepted: jpg | png'}
-              // maxFileSize={5242880}
-            />
+              accept={"accept=jpg,png"}
+              label={"Max file size: 5mb, accepted: jpg | png"}
+              maxFileSize={5242880}
+              withPreview={true}
+              singleImage={true}
+            >
+            {/* <div className="grey">
+            {this.state.selectedFile === null ? null : this.state.selectedFile.name}
+            </div> */}
+            </ImageUploader>
             <button
               type="button"
+              style={{size: "3em", "marginBottom": "0.7em"}}
               className="btn btn-success btn-block"
               onClick={this.onClickHandler}
+              disabled={this.state.disableUpload ? false : true}
             >
               Upload
             </button>
+            {this.state.uploaded === null ? null : this.state.uploaded ? (
+              <Alert
+                variant="success"
+                dismissible={true}
+                onClose={this.resetAlert}
+              >
+                File successfully uploaded!
+              </Alert>
+            ) : (
+              <Alert
+                variant="danger"
+                dismissible={true}
+                onClose={this.resetAlert}
+              >
+                Error: File wasn't uploaded!
+              </Alert>
+            )}
           </div>
         </div>
+        {this.state.loading ? <Loading /> : null}
       </div>
     );
   }
