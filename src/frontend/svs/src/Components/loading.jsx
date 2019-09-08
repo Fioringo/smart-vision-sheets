37eab40c5c32113628bd9b4b1ed763b3ed4c7d66
Fioringo <shtuk.andrey@gmail.com>
2019-09-08 00:03:59 -0400

    added loading screen

---
+++ src/frontend/svs/src/Components/loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+export default class Loading extends React.Component {
+  render() {
+    return (
+      <div className="Loading">
+        <div className="loadingAnimation"></div>
+      </div>
+    );
+  }
+}
