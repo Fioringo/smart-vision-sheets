c969acc778cecb0f8474836c0cb722ac0478cee9
Fioringo <shtuk.andrey@gmail.com>
2019-09-08 06:57:34 -0400

    updated

--- src/frontend/svs/src/Components/home.jsx
+++ src/frontend/svs/src/Components/home.jsx
@@ -1,47 +1,61 @@
 import React from "react";
 import "./home.css";
-const Link = require('react-router-dom').Link;
-let qs = require("query-string")
-const BASE_DOMAIN = process.env.NODE_ENV === "production" ? "" : "http://localhost:5000"
+const Link = require("react-router-dom").Link;
+let qs = require("query-string");
+const BASE_DOMAIN =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
 
 export default class Home extends React.Component {
-
   constructor(props) {
     super(props);
-    const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = localStorage.getItem('refreshToken');
-    const userId = localStorage.getItem('userId');
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+    const userId = localStorage.getItem("userId");
     this.state = {
       loggedIn: accessToken !== null,
       accessToken,
       refreshToken,
-      userId,
+      userId
+    };
+  }
+
+  getHashParams = () => {
+    const hashParams = {};
+    let e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
+    this.resetUrl();
+    return hashParams;
   }
 
   componentDidMount = () => {
-    const params = qs.parse();
+    const params = this.getHashParams();
     console.log(params);
-    const {accessToken, refreshToken, loggedIn } = params;
+    const { accessToken, refreshToken, loggedIn } = params;
     this.setState({
       loggedIn: loggedIn ? true : false,
       accessToken: accessToken,
-      refreshToken: refreshToken,
+      refreshToken: refreshToken
     });
-    if(this.state.loggedIn) {
-      localStorage.setItem("accessToken", this.state.accessToken)
-      localStorage.setItem("refreshToken", this.state.refreshToken)
-      localStorage.setItem("userId", this.state.userId)
+    if (this.state.loggedIn) {
+      localStorage.setItem("accessToken", this.state.accessToken);
+      localStorage.setItem("refreshToken", this.state.refreshToken);
+      localStorage.setItem("userId", this.state.userId);
     }
-    this.resetUrl()
-  }
+    this.resetUrl();
+  };
 
   resetUrl = () => {
-    const currUrl= window.location.href;
-    const resetUrl = currUrl.substring(0, currUrl.lastIndexOf('#'));
+    const currUrl = window.location.href;
+    const resetUrl = currUrl.substring(0, currUrl.lastIndexOf("#"));
     const currState = window.history.state;
     window.history.replaceState(currState, "SVS", resetUrl);
-  }
+  };
 
   render() {
     return (
@@ -58,25 +72,46 @@ export default class Home extends React.Component {
         <div className="description">
           Convert your sheets into Google Sheets
         </div>
-        <br/>
+        <br />
         <div className="smallText">
           Converting your hand written sheets is easy with 4 simple steps:
         </div>
         <div className="homeText">
           <div className="step">
-            <div className="stepTitle">{"Step" } <div className="yellow">1</div>:</div>
-            <div className="stepText"><a href={`${BASE_DOMAIN}/login`} className="link">Login</a> with your Google Account</div>
+            <div className="stepTitle">
+              {"Step"} <div className="yellow">1</div>:
+            </div>
+            <div className="stepText">
+              <a href={`${BASE_DOMAIN}/login`} className="link">
+                Login
+              </a>{" "}
+              with your Google Account
+            </div>
           </div>
           <div className="step">
-            <div className="stepTitle">{"Step" } <div className="blue">2</div>:</div>
-            <div className="stepText">Take a picture of the desired sheet to become a google sheet!</div>
+            <div className="stepTitle">
+              {"Step"} <div className="blue">2</div>:
+            </div>
+            <div className="stepText">
+              Take a picture of the desired sheet to become a google sheet!
+            </div>
           </div>
           <div className="step">
-            <div className="stepTitle">{"Step"}<div className="red">{"3"}</div>:</div>
-            <div className="stepText">Upload the image <Link to="/feature" className="link">here!</Link></div>
+            <div className="stepTitle">
+              {"Step"}
+              <div className="red">{"3"}</div>:
+            </div>
+            <div className="stepText">
+              Upload the image{" "}
+              <Link to="/feature" className="link">
+                here!
+              </Link>
+            </div>
           </div>
           <div className="step">
-            <div className="stepTitle">{"Step" } <div className="green">4</div>:</div>
+            <div className="stepTitle">
+              {"Step"} <div className="green">4</div>:
+            </div>
             <div className="stepText">Enjoy your Google Sheet!</div>
           </div>
         </div>
